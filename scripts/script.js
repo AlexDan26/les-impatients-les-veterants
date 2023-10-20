387018d6fe74c1d3b2b7a5562cfb43b891c8fd3f
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2023-10-20 15:44:42 -0400

    Ajout_CSS_Update_+_Script

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(MotionPathPlugin);
+
+const burger = document.querySelector(".burger");
+
+gsap
+.timeline()
+.to(".buttonDonate",{scale: 1.2, duration: 1.5, yoyo: true, repeat: -1})
+gsap
+.timeline()
+.to(".svg404",{rotate:"720",duration:3, opacity:"100%"})
+
+burger.addEventListener("mouseover", (event) => {
+    gsap
+.timeline()
+.to(".burger",{scale: 1.2, duration: 1.5, yoyo: true})
+});
+
+burger.addEventListener("mouseout", (event) => {
+    gsap
+.timeline()
+.to(".burger",{scale: 1, duration: 1.5, yoyo: true})
+});
