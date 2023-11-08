17908a57f2c3decaae455de05822ad072bf0da93
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2023-11-08 17:02:47 -0500

    Update script.js Swiper 8 Nov

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,27 @@ burger.addEventListener("mouseout", (event) => {
 .to(".burger",{scale: 1, duration: 1.5, yoyo: true})
 });
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 
 let btn_plus    = document.querySelector('.plus');
