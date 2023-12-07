3a278125259b44c95735d790a1a71dd9ea5cbce8
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2023-12-07 16:21:15 -0500

    Update footer.php pour link

--- wordpress/wp-content/themes/theme-de-base/footer.php
+++ wordpress/wp-content/themes/theme-de-base/footer.php
@@ -9,12 +9,18 @@
 <footer>
 	<p>Développement web - Collège Montmorency - 2021</p>
 </footer>
-
 <?php wp_footer(); 
 /* Espace où WordPress peut insérer des fichiers .js et autres. Par exemple pour des extensions (plugins). 
 	 Si vous enlevez cette fonction, vous désactiverez du même coup toutes vos extensions (plugins) 🤷. 
 	 Vous pouvez la déplacer si désiré, mais garder là. */
+	 
 ?>
 
 </body>
+<foot>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/MotionPathPlugin.min.js"></script>
+<script src="scripts/script.js"></script>
+</foot>
 </html>
