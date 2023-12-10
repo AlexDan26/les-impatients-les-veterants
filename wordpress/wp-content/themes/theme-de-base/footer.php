20d792164949e2cafee79c3ef8978551140acc2d
Penelope-M <90867349+Penelope-M@users.noreply.github.com>
2023-12-10 15:16:31 -0500

    menu php

--- wordpress/wp-content/themes/theme-de-base/footer.php
+++ wordpress/wp-content/themes/theme-de-base/footer.php
@@ -10,10 +10,14 @@
 	<p>Développement web - Collège Montmorency - 2021</p>
 </footer>
 <?php wp_footer(); 
+
 /* Espace où WordPress peut insérer des fichiers .js et autres. Par exemple pour des extensions (plugins). 
 	 Si vous enlevez cette fonction, vous désactiverez du même coup toutes vos extensions (plugins) 🤷. 
 	 Vous pouvez la déplacer si désiré, mais garder là. */
 	 
+?>
+<?php 
+  wp_nav_menu(array('theme_location' => 'footer-menu'));
 ?>
 
 </body>
