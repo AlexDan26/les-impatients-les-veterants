5d4a452d2be4bf0be6268d0bbf1c6fc1513659d0
Penelope-M <90867349+Penelope-M@users.noreply.github.com>
2023-12-12 01:02:55 -0500

    Update news-hub.php

--- wordpress/wp-content/themes/theme-de-base/news-hub.php
+++ wordpress/wp-content/themes/theme-de-base/news-hub.php
@@ -6,6 +6,7 @@
 get_header(); // Affiche header.php
 $nouvelles = new WP_Query(array('post_type' => 'nouvelles'));
 ?>
+
 <main>  
     <div class="container">
         <h1 class="top_title">Les impatients</h1>
@@ -21,25 +22,21 @@ $nouvelles = new WP_Query(array('post_type' => 'nouvelles'));
         <div class="row">
             <!-- CARD NOUVELLE -->
             <?php
-            if ($nouvelles->have_posts()) :
-                while ($nouvelles->have_posts()) : $nouvelles->the_post();
+            while ($nouvelles->have_posts()) : $nouvelles->the_post();
             ?>
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card first">
-                            <img class="card-img-top" src="<?php the_field('logo'); ?>">
-                            <div class="card-body bleu">
-                                <h3 class="card-title"><?php the_field('Titre'); ?></h3>
-                                <p class="card-text"><?php the_field('Description'); ?></p>
-                                <a class="navbar-brand stretched-link" href="<?php the_permalink(); ?>"></a>
-                            </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card first">
+                        <img class="card-img-top" src="<?php the_field('logo'); ?>">
+                        <div class="card-body bleu">
+                            <h3 class="card-title"><?php the_field('Titre'); ?></h3>
+                            <p class="card-text"><?php the_field('Description'); ?></p>
+                            <a class="navbar-brand stretched-link" href="<?php the_permalink(); ?>"></a>
                         </div>
                     </div>
+                </div>
             <?php
-                endwhile;
-                wp_reset_postdata(); // Fermeture de la boucle
-            else : // Si aucune page n'a été trouvée
-                get_template_part('partials/404'); // Affiche partials/404.php
-            endif;
+            endwhile;
+            wp_reset_postdata(); // Fermeture de la boucle
             ?>
         </div>
     </div>
@@ -49,3 +46,4 @@ $nouvelles = new WP_Query(array('post_type' => 'nouvelles'));
 get_sidebar(); // Affiche le contenu de sidebar.php
 get_footer(); // Affiche footer.php 
 ?>
+
