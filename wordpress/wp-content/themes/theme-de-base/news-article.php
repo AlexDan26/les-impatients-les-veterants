acf35435778d88958bca5d23836fc37d52a47295
Penelope-M <90867349+Penelope-M@users.noreply.github.com>
2023-12-11 21:08:37 -0500

    Update news-article.php

--- wordpress/wp-content/themes/theme-de-base/news-article.php
+++ wordpress/wp-content/themes/theme-de-base/news-article.php
@@ -3,19 +3,22 @@
  * 	Template Name: Article de nouvelles
  */
 get_header(); // Affiche header.php
-
-if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
-	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
-	while ( have_posts() ) : the_post(); 
+$nouvelles = new WP_Query('post_type=nouvelles');
 ?>
 
 	<article>
+<?php
+	if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
+	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
+	while ( $nouvelles->have_posts() ) : $nouvelles->the_post(); 
+?>
 		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
 			<h2>
 				<?php the_title(); // Titre de la page ?>
 			</h2>
 		<?php endif; ?>
-		
+		<!--IMAGES ARTICLES-->
+        <img itemprop="image" class="hero" /><?php the_post_thumbnail(); ?><img />
 		<?php the_content(); // Contenu principal de la page ?>
 	</article>
 <?php endwhile; // Fermeture de la boucle
