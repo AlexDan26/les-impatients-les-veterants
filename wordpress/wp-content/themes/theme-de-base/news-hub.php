aeb560fbae80b1e773b3b1cddbf3dfb6e60f8df3
Penelope-M <90867349+Penelope-M@users.noreply.github.com>
2023-12-14 15:05:11 -0500

    hub nouvelles

--- wordpress/wp-content/themes/theme-de-base/news-hub.php
+++ wordpress/wp-content/themes/theme-de-base/news-hub.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  *  Template Name: Hub de nouvelles
@@ -10,47 +9,46 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
     // Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
     while ( have_posts() ) : the_post();
 ?>
- 
+
 <article>
-<div class="container">
-<h1 class="top_title">Les impatients </h1>
-    <div class="hero main-container">
-      <!--Le titre de la page-->
-      <div class="first-container share">
-        <h1><span id="one">N</span><span>o</span><span>s</span>
-            <span>N</span><span>o</span><span>u</span><span>v</span><span>e</span><span>l</span><span>l</span><span>e</span><span>s</span>
-        </h1>
-      </div>
-    </div>
- 
-<div class="row">
-   
-        <?php
-  $nouvelles = new WP_Query('post_type=nouvelles');
+    <div class="container">
+        <h1 class="top_title">Les impatients </h1>
+        <div class="hero main-container">
+            <!--Le titre de la page-->
+            <div class="first-container share">
+                <h1><span id="one">N</span><span>o</span><span>s</span>
+                    <span>N</span><span>o</span><span>u</span><span>v</span><span>e</span><span>l</span><span>l</span><span>e</span><span>s</span>
+                </h1>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <?php
+   $nouvelles = new WP_Query(array('post_type'=>'nouvelle', "posts_per_page"=>6));
   while ($nouvelles->have_posts()) : $nouvelles->the_post();
 ?>
-        <!-- 👆 Début boucle while -->
-        <div class="row">
-        <!--CARD NOUVELLE-->
-        <div class="col-12 col-md-6 col-lg-4">
-          <div class="card first">
-            <img class="card-img-top" src="<?php /*MET LE NOM DE TON FEILD IMAGE ICI*/ ?>">
-            <div class="card-body bleu">
-              <h3 class="card-title"><?php the_field('titre'); ?></h3>
-              <p class="card-text"><?php the_field('description'); ?></p>
-              <a class="navbar-brand stretched-link" href="nouvelles.html"></a>
-            </div>
-          </div>
+            <!-- 👆 Début boucle while -->
+           
+                <!--CARD NOUVELLE-->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card <?php the_field('first'); ?>">
+                        <img class="card-img-top" src="<?php the_post_thumbnail('medium'); ?>
+                        <div class="card-body <?php the_field('couleur'); ?>">
+                            <h3 class="card-title"><?php the_field('titre'); ?></h3>
+                            <p class="card-text"><?php the_field('description'); ?></p>
+                            <a class="navbar-brand stretched-link" href="<?php the_permalink(); ?>"></a>
+                        </div>
+                    </div>
+                </div>
+
+
+            <!-- 👇 Fin boucle while -->
+            <?php endwhile;
+            wp_reset_postdata();
+            ?>
         </div>
-        
-        <!-- 👇 Fin boucle while -->
-        <?php
-  endwhile;
-  wp_reset_postdata();
-?>
-   
-    </div>
-    </div>
+    
 </article>
 <?php endwhile; // Fermeture de la boucle
  
@@ -58,6 +56,6 @@ else : // Si aucune page n'a été trouvée
     get_template_part( 'partials/404' ); // Affiche partials/404.php
 endif;
  
-get_sidebar(); // Affiche le contenu de sidebar.php
+
 get_footer(); // Affiche footer.php
 ?>
